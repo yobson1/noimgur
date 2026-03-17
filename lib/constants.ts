4846ff4eafcde8054a950d98fe9458ac6f5d8486
brad <contact@yobson.xyz>
2026-03-17 21:25:19 +0000

    rotate interval to 60m

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const IMGUR_REGEX = /https?:\/\/(?:i\.)?imgur\.com\//;
 export const IMGUR_REGEX_CAPTURE = 'https?://(?:i\\.)?imgur\\.com/(.*)';
 export const API_URL = 'https://rimgo.codeberg.page/api.json';
 export const RULE_ID = 1;
-export const ROTATE_INTERVAL_MINUTES = 240;
+export const ROTATE_INTERVAL_MINUTES = 60;
 export const MAX_STATE_AGE_MS = ROTATE_INTERVAL_MINUTES * 60 * 1000;
 export const ALARM_NAME = 'rotateInstance';
 
